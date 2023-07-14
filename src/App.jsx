e52104f75e02226bc4b67bd0613a1e3d5c74c509
RULI RIPALDI <rullyriel@gmail.com>
2023-07-14 11:19:42 +0700

    komit awal

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react' 
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline">
+        Welcome 🙋‍♂️
+      </h1>
+    </>
+  )
+}
+
+export default App
